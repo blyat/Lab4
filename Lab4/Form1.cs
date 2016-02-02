f82364bce6fb2f5408838df98abc603768969515
blyat <daniel.bermude@gmail.com>
2016-02-01 20:13:23 -0600

    v1.0
    Lab de condiciones

---
+++ Lab4/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab4
+{
+    public partial class Form1 : Form
+    {
+        bool prueba = false;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (prueba == false)
+            {
+                textBox1.Text = "Hello1";
+            }
+            else{
+                textBox1.Text = "Hello2";
+            }
+        }
+    }
+}
